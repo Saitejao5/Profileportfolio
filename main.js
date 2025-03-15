ad5f4587ae336c5e39399bb8fce9ec8bac12d91d
Saitejao5 <166217076+Saitejao5@users.noreply.github.com>
2025-03-15 11:52:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,35 @@
+
 function switchTab(tabId, element) {
-  // Hide all content sections
-  document.querySelectorAll('.tab-content').forEach(tab => {
-      tab.classList.remove("active");
-  });
+    // Hide all content sections
+    document.querySelectorAll('.tab-content').forEach(tab => {
+        tab.classList.remove("active");
+    });
 
-  // Remove active state from all buttons
-  document.querySelectorAll('.tab-button').forEach(button => {
-      button.classList.remove("active");
-  });
+    // Remove active state from all buttons
+    document.querySelectorAll('.tab-button').forEach(button => {
+        button.classList.remove("active");
+    });
 
-  // Show the selected content & highlight the clicked button
-  document.getElementById(tabId).classList.add("active");
-  element.classList.add("active");
+    // Show the selected content & highlight the clicked button
+    document.getElementById(tabId).classList.add("active");
+    element.classList.add("active");
 }
 
 function genai() {
-window.open("genai.jpg");
-// Tab to edit
+  window.open("genai.jpg");
+  // Tab to edit
 }
 
 function tel(){
-window.open("tel.jpg");
-}
-
+  window.open("tel.jpg");
+  }
+  
 function digi() {
-window.open("digi.jpg")
-// Tab to edit
+  window.open("digi.jpg")
+  // Tab to edit
 }
-
-
-
+function toggleMenu() {
+            document.querySelector(".off-canvas").classList.toggle("active");
+                        document.querySelector(".menu-btn").classList.toggle("active");
+        }
+        
